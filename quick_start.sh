011070681a47422f3808a39142c7b333ba2c4e00
Johnny Gaffey <john@socialgrowthtechnologies.com>
2014-04-14 11:49:55 -0700

    added a quickstart script

---
+++ quick_start.sh
@@ -0,0 +1,10 @@
+sudo apt-get -y install git
+cd ..
+git clone https://github.com/johnnygaffey/dotfiles.git
+cd dotfiles/
+bash install.sh
+wget -O - http://bootstrap.saltstack.org | sudo sh
+cd ~/
+sudo ln -s /home/johnnyg/salt-states /srv/salt
+sudo vim /etc/salt/minion
+sudo salt-call state.highstate test=True
