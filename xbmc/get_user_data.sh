c5e2cc17c303c779acb3d97a7ab705377abf5317
Johnny Gaffey <john@socialgrowthtechnologies.com>
2014-04-22 20:19:23 -0700

    changing dir for xbmc user data

--- xbmc/get_user_data.sh
+++ xbmc/get_user_data.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mkdir /home/johnnyg/.xbmc/
+if [ ! -d "/home/johnnyg/.xbmc/"]; then
+    sudo mkdir /home/johnnyg/.xbmc/
+fi
 cd /home/johnnyg/.xbmc/
-rm -r /home/johnnyg/.xbmc/userdata
-wget - https://www.dropbox.com/sh/1jqd5sxtjve2zv1/f6jOF9A0Ab/userdata.tar.gz
-tar -zxvf userdata.tar.gz
-rm userdata.tar.gz
+if [ -d "/home/johnnyg/.xbmc/userdata"]; then
+    sudo rm -r /home/johnnyg/.xbmc/userdata
+fi
+sudo wget - https://www.dropbox.com/sh/1jqd5sxtjve2zv1/f6jOF9A0Ab/userdata.tar.gz
+sudo tar -zxvf userdata.tar.gz
+sudo rm userdata.tar.gz
