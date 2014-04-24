b21e198f50e6e0ce698e68cb199ec26ed6a8ed0e
Johnny Gaffey <john@socialgrowthtechnologies.com>
2014-04-23 18:59:26 -0700

    couple quick changes to the quick start script

--- quick_start.sh
+++ quick_start.sh
@@ -1,13 +1,13 @@
-sudo apt-get -y install git
-sudo apt-get -y install vim
+#sudo apt-get -y install git
+#sudo apt-get -y install vim
 cd ~/
 git clone https://github.com/johnnygaffey/dotfiles.git
 cd ~/dotfiles/
-bash install.sh
+bash install.sh -b
 cd ~/salt-states/xbmc/
 wget - https://www.dropbox.com/s/hh7afxftgl5tt4m/userdata.tar.gz
 cd ~/
 wget -O - http://bootstrap.saltstack.org | sudo sh
 sudo ln -s /home/johnnyg/salt-states /srv/salt
 #sudo vim /etc/salt/minion
-sudo salt-call --local state.highstate test=True
+sudo salt-call --local state.highstate #test=True
