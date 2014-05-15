72450ca5f0e15618727a6f117013016e73445eee
Johnny Gaffey <john@socialgrowthtechnologies.com>
2014-05-14 23:53:48 -0400

    removed old xbmc user data dl

--- quick_start.sh
+++ quick_start.sh
@@ -1,6 +1,5 @@
 cd ~/
 git clone https://SGTJohnny@Bitbucket.org/SGTJohnny/salt-pillar.git
-wget - https://www.dropbox.com/s/hh7afxftgl5tt4m/userdata.tar.gz
 wget -O - http://bootstrap.saltstack.org | sudo sh
 sudo ln -s /home/johnnyg/salt-states /srv/salt
 sudo salt-call --local state.highstate #test=True
