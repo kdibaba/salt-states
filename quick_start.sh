70ac6a7357444d651b61c163e797b79bae17cd5e
Johnny Gaffey <john@socialgrowthtechnologies.com>
2014-05-17 11:47:11 -0400

    salt-states will pull via ssh now too

--- quick_start.sh
+++ quick_start.sh
@@ -2,5 +2,7 @@ cd ~/
 git clone https://SGTJohnny@Bitbucket.org/SGTJohnny/salt-pillar.git
 wget -O - http://bootstrap.saltstack.org | sudo sh
 sudo ln -s /home/johnnyg/salt-states /srv/salt
+cd ~/salt-states/
+git remote set-url origin git@github.com:johnnygaffey/salt-states.git
 sudo salt-call --local state.highstate #test=True
 
