d46548888cb226d579dbae6d988302c34bb481d1
johnny <johnnygaffey@gmail.com>
2014-05-17 11:26:04 -0700

    i think this should fix the ssh key issue.

--- quick_start.sh
+++ quick_start.sh
@@ -4,6 +4,6 @@ wget -O - http://bootstrap.saltstack.org | sudo sh
 sudo ln -s /home/johnnyg/salt-states /srv/salt
 cd ~/salt-states/
 git remote set-url origin git@github.com:johnnygaffey/salt-states.git
-sudo salt-call --local state.sls core.ssh
+ssh-keygen
 sudo salt-call --local state.highstate #test=True
 
