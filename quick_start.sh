f2b92bb73551ba8a519f38657a5b0f971d9d0d15
Johnny Gaffey <john@socialgrowthtechnologies.com>
2014-05-19 14:19:43 -0400

    going to be be testing to see how to get this working in 1 run.

--- quick_start.sh
+++ quick_start.sh
@@ -4,7 +4,5 @@ wget -O - http://bootstrap.saltstack.org | sudo sh
 sudo ln -s /home/johnnyg/salt-states /srv/salt
 cd ~/salt-states/
 git remote set-url origin git@github.com:johnnygaffey/salt-states.git
-ssh-keygen
-sudo salt-call --local state.sls core.ssh #why do i need to do this ?
-sudo salt-call --local state.highstate #test=True
+#sudo salt-call --local state.highstate #test=True
 
